51c6cbf2bd78e4c75931a2494c5386771f3f2e1e
LeeHank <ckshoupon@hotmail.com>
2015-03-21 12:16:49 +0800

    Second tried
    add some description, for practicing

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,10 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-        I <- NULL
+        i <- NULL
+        set <- function(y){
+        x <<- y
+        i <<- NULL
 }
 
 
